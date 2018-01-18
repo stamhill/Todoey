16b12c965ab7d2c8e8bfee398a03980fe08f3ab3
Stam Hill <eshcom1@yahoo.com>
2018-01-17 18:31:16 -0800

    Convert to MVC Design Pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Stam Hill on 1/17/18.
+//  Copyright © 2018 Stam Hill. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
